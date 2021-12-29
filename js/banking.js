f7d1ad2ec3a349210b07de980d81986b65a3d95f
Jubayer7 <sonnibai7@gmail.com>
2021-12-29 18:18:31 +0600

    Remain just responsive

--- js/banking.js
+++ js/banking.js
@@ -1,9 +1,11 @@
 document.getElementById('deposite-btn').addEventListener('click',function(){
     const depositInput = document.getElementById('deposit-input')
+    console.log(depositInput);
    const newDepositAmount = parseFloat(depositInput.value)
 
 //    Add amount to deposit
    const depositTotal = document.getElementById('deposit-total')
+   console.log(depositTotal);
    const previousDepositText = parseFloat(depositTotal.innerText)
     const newDepositTotal = newDepositAmount + previousDepositText;
 
@@ -23,4 +25,32 @@ document.getElementById('deposite-btn').addEventListener('click',function(){
     //  remove input field
     depositInput.value = '';
  
+})
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+
+    const withdrawInput = document.getElementById('withdraw-input')
+    const withdrawAmount = parseFloat(withdrawInput.value)
+    
+    const PreviousAmount = document.getElementById('withdraw-total')
+    const withdrawTotal = parseFloat(PreviousAmount.innerText)
+
+    const totalWithdraw = withdrawTotal + withdrawAmount;
+
+    PreviousAmount.innerText =totalWithdraw;
+
+    // reduce total balance
+    
+    const totalBalanceText = document.getElementById('total-balance')
+    console.log(totalBalanceText)
+    const preTotalBalance= parseFloat(totalBalanceText.innerText)
+    console.log(preTotalBalance)
+    const currentTotalBalance = preTotalBalance - withdrawAmount;
+
+    totalBalanceText.innerText = currentTotalBalance;
+
+
+
+    // remove input field
+    withdrawInput.value = '';
 })
